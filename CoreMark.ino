637800fb51109c53f5076e30e4112a7c63fdec8f
GunterO <gunter@otte.be>
2020-11-30 16:45:30 +0100

    Make use of the 2 cores of the ESP32

--- CoreMark.ino
+++ CoreMark.ino
@@ -1,14 +1,31 @@
+/*
+ * core 0: used for wifi, BT, ...
+ * core 1: where a *normal* program runs
+*/
+
 // CoreMark Benchmark for Arduino compatible boards
 //   original CoreMark code: https://github.com/eembc/coremark
-
+#include "Arduino.h"
 #include <stdarg.h>
+#include <esp_task_wdt.h>
+
+#define TWDT_TIMEOUT_S 20
+
+TaskHandle_t Task0;
+TaskHandle_t Task1;
 
 // A way to call the C-only coremark function from Arduino's C++ environment
 extern "C" int coremark_main(void);
 
 void setup()
 {
-	Serial.begin(9600); 
+  // Set the watchdog to 20s (enough to finish the benchmark, and make sure it won't panic)
+  esp_task_wdt_init(TWDT_TIMEOUT_S, false); // (Timeout period of TWDT in seconds, Panic aka reboot)
+
+  // Disable the watchdogs
+  //disableCore0WDT(); disableCore1WDT(); disableLoopWDT();
+  
+	Serial.begin(115200); 
 	while (!Serial) ; // wait for Arduino Serial Monitor
 	delay(500);
 	Serial.println("CoreMark Performance Benchmark");
@@ -17,14 +34,57 @@ void setup()
 	Serial.println("lists, compute matrix multiply, and execute state machine code.");
 	Serial.println();
 	Serial.println("Iterations/Sec is the main benchmark result, higher numbers are better");
-	Serial.println("Running.... (usually requires 12 to 20 seconds)");
-	Serial.println();
-	delay(250);
+//	Serial.println("Running.... (usually requires 12 to 20 seconds)");
+//	Serial.println();
+
+  // Dual core code
+  xTaskCreatePinnedToCore(
+    loop0, /* Function to implement the task */
+    "Task0", /* Name of the task */
+    10000, /* Stack size in words */
+    NULL, /* Task input parameter */
+    20, /* Priority of the task */
+    &Task0, /* Task handle */
+    0); /* Core where the task should run */
+  delay(500); // Wait 500ms to make sure the output is displayed nicely. Because the cores are running at the same speed, the output would be scambled.
+  xTaskCreatePinnedToCore(
+    loop1, /* Function to implement the task */
+    "Task1", /* Name of the task */
+    10000, /* Stack size in words */
+    NULL, /* Task input parameter */
+    20, /* Priority of the task */
+    &Task1, /* Task handle */
+    1); /* Core where the task should run */
+
+/*
+  // Original, single core code
+  Serial.println("Running.... (usually requires 12 to 20 seconds)");
+  Serial.println();
+  delay(250);
 	coremark_main(); // Run the benchmark  :-)
+*/
+}
+
+void loop0(void * parameter)
+{
+  Serial.println("Running Task0.... (usually requires 12 to 20 seconds)");
+  coremark_main(); // Run the benchmark  :-)
+  Serial.println("Task0 finished.");
+  vTaskDelete(Task0);
+}
+
+void loop1(void * parameter)
+{
+  Serial.println("Running Task1.... (usually requires 12 to 20 seconds)");
+  coremark_main(); // Run the benchmark  :-)
+  Serial.println("Task1 finished.");
+  vTaskDelete(Task1);
 }
 
 void loop()
 {
+  // Delete the task which was used for this loop
+  vTaskDelete(NULL);
 }
 
 // CoreMark calls this function to print results.
